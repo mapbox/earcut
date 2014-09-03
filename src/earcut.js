8abfe71e4f8ebbd331d38492f212e61308bc6509
Vladimir Agafonkin <agafonkin@gmail.com>
2014-09-03 13:38:24 +0300

    fixes and improvements

--- src/earcut.js
+++ src/earcut.js
@@ -1,9 +1,7 @@
 
 // module.exports = earcut;
 
-// var Polygon = require('./polygon');
-
-function Node(data, related, status) {
+function PolygonNode(data, related, status) {
     this.data = data;
 
     this.related = related;
@@ -19,11 +17,8 @@ function Node(data, related, status) {
 }
 
 function Polygon() {
-    this.length = 0;
     this.last = null;
 }
-
-Polygon.Node = Node;
 
 Polygon.prototype = {
     insert: function (node) {
@@ -31,17 +26,13 @@ Polygon.prototype = {
             node.prev = node;
             node.next = node;
         } else {
-            this.insertAfter(node, this.last);
+            var last = this.last;
+            node.next = last.next;
+            node.prev = last;
+            last.next.prev = node;
+            last.next = node;
         }
         this.last = node;
-        this.length++;
-    },
-
-    insertAfter: function (newNode, node) {
-        newNode.next = node.next;
-        newNode.prev = node;
-        node.next.prev = newNode;
-        node.next = newNode;
     },
 
     remove: function (node) {
@@ -51,9 +42,12 @@ Polygon.prototype = {
             node.prev.next = node.next;
             if (node === this.last) this.last = node.prev;
         }
-        this.length--;
     }
 };
+
+function windPart(p1, p2) {
+    return (p2[0] - p1[0]) * (p2[1] + p1[1]);
+}
 
 function earcut(points) {
 
@@ -62,25 +56,25 @@ function earcut(points) {
         ears = new Polygon(),
         reflex = new Polygon();
 
+    var sum = 0;
+
     // create a doubly linked list from polygon points
     for (var i = 0; i < points.length; i++) {
-        var node = new Polygon.Node(points[i]);
+        var node = new PolygonNode(points[i]);
         contour.insert(node);
+
+        var j = i ? i - 1 : points.length - 1;
+        sum += windPart(points[j], points[i]);
+
     }
 
-    // TODO detect clockwise
-    var clockwise = false;
+    var clockwise = sum < 0;
 
     // create 2 doubly linked lists, one containing convex vertices and other containing reflex vertices
     var node = contour.last;
     do {
-        // var angle = getAngle(node.data, node.prev.data, node.next.data);
-
-        if (isConvex(node, clockwise)) {
-            convex.insert(new Polygon.Node(null, node, 2));
-        } else {
-            reflex.insert(new Polygon.Node(null, node, 1));
-        }
+        if (isConvex(node, clockwise)) convex.insert(new PolygonNode(null, node, 2));
+        else reflex.insert(new PolygonNode(null, node, 1));
 
         node = node.next;
     } while (node !== contour.last);
@@ -88,31 +82,28 @@ function earcut(points) {
     // create a dooubly linked list of ears
     var node = convex.last;
     do {
-        if (isEar(node.related, reflex)) ears.insert(new Polygon.Node(node.data, node.related, 3));
+        if (isEar(node.related, reflex)) ears.insert(new PolygonNode(null, node.related, 3));
         node = node.next;
     } while (node !== convex.last);
 
     var triangles = [];
 
     // iterate through ears, slicing them one by one
-    var node = ears.last;
+    var ear = ears.last;
     do {
+        var node = ear.related,
+            prev = node.prev,
+            next = node.next;
 
-        var contourNode = node.related;
+        triangles.push([prev.data, node.data, next.data]);
 
-        triangles.push([contourNode.prev.data, contourNode.data, contourNode.next.data]);
-
-        ears.remove(node);
-
-        var prev = contourNode.prev,
-            next = contourNode.next;
-
-        contour.remove(contourNode);
+        ears.remove(ear);
+        contour.remove(node);
 
         reclassifyEar(prev, reflex, ears, clockwise);
         reclassifyEar(next, reflex, ears, clockwise);
 
-        node = ears.last;
+        ear = ears.last;
     } while (ears.last);
 
     return triangles;
@@ -124,9 +115,8 @@ function reclassifyEar(node, reflex, ears, clockwise) {
         if (isConvex(node, clockwise)) {
             reflex.remove(node.related); // angle was reflex and became convex
             node.status = 2;
-        } else {
-            return; // angle remained reflex, no ear status change
-        }
+
+        } else return; // angle remained reflex, no ear status change
     }
 
     var earNow = isEar(node, reflex);
@@ -134,7 +124,6 @@ function reclassifyEar(node, reflex, ears, clockwise) {
     if (node.status === 2) { // convex
         if (earNow) { // became ear
             node.status = 3;
-            // node.related.data = getAngle(node.data, node.prev.data, node.next.data);
             ears.insert(node.related);
         }
     } else { // ear
@@ -146,15 +135,15 @@ function reclassifyEar(node, reflex, ears, clockwise) {
 }
 
 function isConvex(node, clockwise) {
-    return clockwise === (getAngle(node.next.data, node.prev.data, node.data) >= 0);
+    return clockwise === (cross(node.next.data, node.prev.data, node.data) >= 0);
 }
 
-function getAngle(p, a, b) {
+function cross(p, a, b) {
     var ax = a[0] - p[0],
         ay = a[1] - p[1],
         bx = b[0] - p[0],
         by = b[1] - p[1];
-    return Math.atan2(ax * by - ay * bx, ax * by + ay * bx);
+    return ax * by - ay * bx;
 }
 
 function isEar(node, reflex) {
@@ -164,7 +153,8 @@ function isEar(node, reflex) {
     if (!node2) return true;
     do {
         rel = node2.related;
-        if (node.prev !== rel && node.next !== rel && inside(rel, node)) return false;
+        if (node.prev !== rel && node.next !== rel &&
+                inside(rel.data, node.prev.data, node.data, node.next.data)) return false;
         node2 = node2.next;
     } while (node2 !== reflex.last);
 
@@ -172,16 +162,11 @@ function isEar(node, reflex) {
 }
 
 
-function inside(node, convex) {
-    var p = node.data,
-        a = convex.prev.data,
-        b = convex.data,
-        c = convex.next.data,
-
-        A = -b[1] * c[0] + a[1] * (-b[0] + c[0]) + a[0] * (b[1] - c[1]) + b[0] * c[1],
+function inside(p, a, b, c) {
+    var A = -b[1] * c[0] + a[1] * (-b[0] + c[0]) + a[0] * (b[1] - c[1]) + b[0] * c[1],
         sign = A < 0 ? -1 : 1,
         s = (a[1] * c[0] - a[0] * c[1] + (c[1] - a[1]) * p[0] + (a[0] - c[0]) * p[1]) * sign,
         t = (a[0] * b[1] - a[1] * b[0] + (a[1] - b[1]) * p[0] + (b[0] - a[0]) * p[1]) * sign;
 
-    return s > 0 && t > 0 && (s + t) < A * sign;
+    return s >= 0 && t >= 0 && (s + t) <= A * sign;
 }
