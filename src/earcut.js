7133e877dcb9303fc074daecffe476924454a96f
Francisco Avila <elfrank.ab@gmail.com>
2016-03-09 12:10:23 -0800

    Support AMD, Browserify and Window object
    Extend support for library so it can be used outside Browserify.

--- src/earcut.js
+++ src/earcut.js
@@ -1,6 +1,15 @@
 'use strict';
 
-module.exports = earcut;
+if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(function () {
+        return earcut;
+    });
+} else if (typeof module !== 'undefined' && module.exports) {
+    module.exports = earcut;
+} else {
+    window.earcut = earcut;
+}
 
 function earcut(data, holeIndices, dim) {
 
