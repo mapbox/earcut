ee1adb0675f6c415f41bfb65079dd0eea06bf194
Vladimir Agafonkin <agafonkin@gmail.com>
2016-03-15 19:46:40 +0200

    filter out the closing point, close #52

--- src/earcut.js
+++ src/earcut.js
@@ -43,11 +43,18 @@ function earcut(data, holeIndices, dim) {
 // create a circular doubly linked list from polygon points in the specified winding order
 function linkedList(data, start, end, dim, clockwise) {
     var i, last;
+
     if (clockwise === (signedArea(data, start, end, dim) > 0)) {
         for (i = start; i < end; i += dim) last = insertNode(i, data[i], data[i + 1], last);
     } else {
         for (i = end - dim; i >= start; i -= dim) last = insertNode(i, data[i], data[i + 1], last);
     }
+
+    if (last && equals(last, last.next)) {
+        removeNode(last);
+        last = last.next;
+    }
+
     return last;
 }
 
