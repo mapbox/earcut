b85bcf2790d8e5644bcbc72a29ccb104c40ea27c
Rocky Wei <finscn@gmail.com>
2016-12-13 01:38:06 +0800

    Avoid T-junction

--- src/earcut.js
+++ src/earcut.js
@@ -453,9 +453,9 @@ function getLeftmost(start) {
 
 // check if a point lies within a convex triangle
 function pointInTriangle(ax, ay, bx, by, cx, cy, px, py) {
-    return (cx - px) * (ay - py) - (ax - px) * (cy - py) >= 0 &&
-           (ax - px) * (by - py) - (bx - px) * (ay - py) >= 0 &&
-           (bx - px) * (cy - py) - (cx - px) * (by - py) >= 0;
+    return (cx - px) * (ay - py) - (ax - px) * (cy - py) > 0 &&
+           (ax - px) * (by - py) - (bx - px) * (ay - py) > 0 &&
+           (bx - px) * (cy - py) - (cx - px) * (by - py) > 0;
 }
 
 // check if a diagonal between two polygon nodes is valid (lies in polygon interior)
