3c9e318aeebe24c11a342f5df8b7ef25b95d0814
Adrian Leonhard <adrianleonhard@gmail.com>
2017-10-08 21:43:29 +0200

    Made holes optional.

--- index.d.ts
+++ index.d.ts
@@ -9,7 +9,7 @@
  * @example with a hole: earcut([0,0, 100,0, 100,100, 0,100,  20,20, 80,20, 80,80, 20,80], [4]); // [3,0,4, 5,4,0, 3,4,7, 5,0,1, 2,3,7, 6,5,1, 2,7,6, 6,1,2]
  * @example with 3d coords: earcut([10,0,1, 0,50,2, 60,60,3, 70,10,4], null, 3); // [1,0,3, 3,2,1]
  */
-declare function earcut(vertices: ArrayLike<number>, holes: ArrayLike<number>, dimensions?: number): number[]
+declare function earcut(vertices: ArrayLike<number>, holes?: ArrayLike<number>, dimensions?: number): number[]
 declare const exports: typeof earcut & { default: typeof earcut }
 export = exports
 export as namespace earcut
