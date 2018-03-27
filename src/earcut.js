6376341690dee2653b31a82714c52dbd4daaef43
Russ Biggs <russbiggs@gmail.com>
2018-03-27 07:27:30 -0600

    Replace vertice with vertex in documentation (#97)
    * updated documentation and comments to replace vertice (singular of vertices) with correct form, vertex fixes #80
    
    * formatting fixes
    
    * indentation fixes

--- src/earcut.js
+++ src/earcut.js
@@ -107,7 +107,7 @@ function earcutLinked(ear, triangles, dim, minX, minY, invSize, pass) {
 
             removeNode(ear);
 
-            // skipping the next vertice leads to less sliver triangles
+            // skipping the next vertex leads to less sliver triangles
             ear = next.next;
             stop = next.next;
 
@@ -571,14 +571,14 @@ function removeNode(p) {
 }
 
 function Node(i, x, y) {
-    // vertice index in coordinates array
+    // vertex index in coordinates array
     this.i = i;
 
     // vertex coordinates
     this.x = x;
     this.y = y;
 
-    // previous and next vertice nodes in a polygon ring
+    // previous and next vertex nodes in a polygon ring
     this.prev = null;
     this.next = null;
 
