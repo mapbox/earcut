211596cd4348e605d5356a0daf7060ce0215620e
Vladimir Agafonkin <agafonkin@gmail.com>
2020-01-21 15:58:23 +0200

    fix another race condition, close #131 (#132)

--- src/earcut.js
+++ src/earcut.js
@@ -293,6 +293,9 @@ function eliminateHole(hole, outerNode) {
     outerNode = findHoleBridge(hole, outerNode);
     if (outerNode) {
         var b = splitPolygon(outerNode, hole);
+
+        // filter collinear points around the cuts
+        filterPoints(outerNode, outerNode.next);
         filterPoints(b, b.next);
     }
 }
