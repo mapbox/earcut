8cf38c4db634acfb1c73057c418e12ec32b43fcb
Peter Boyer <peter.b.boyer@gmail.com>
2021-01-24 23:43:36 -0500

    store vertices in local variables rather than derefing every loop

--- src/earcut.js
+++ src/earcut.js
@@ -148,8 +148,15 @@ function isEar(ear) {
     // now make sure we don't have other points inside the potential ear
     var p = ear.next.next;
 
+    var ax = a.x,
+        ay = a.y,
+        bx = b.x,
+        by = b.y,
+        cx = c.x,
+        cy = c.y;
+
     while (p !== ear.prev) {
-        if (pointInTriangle(a.x, a.y, b.x, b.y, c.x, c.y, p.x, p.y) &&
+        if (pointInTriangle(ax, ay, bx, by, cx, cy, p.x, p.y) &&
             area(p.prev, p, p.next) >= 0) return false;
         p = p.next;
     }
@@ -164,11 +171,18 @@ function isEarHashed(ear, minX, minY, invSize) {
 
     if (area(a, b, c) >= 0) return false; // reflex, can't be an ear
 
+    var ax = a.x,
+        ay = a.y,
+        bx = b.x,
+        by = b.y,
+        cx = c.x,
+        cy = c.y;
+
     // triangle bbox; min & max are calculated like this for speed
-    var minTX = a.x < b.x ? (a.x < c.x ? a.x : c.x) : (b.x < c.x ? b.x : c.x),
-        minTY = a.y < b.y ? (a.y < c.y ? a.y : c.y) : (b.y < c.y ? b.y : c.y),
-        maxTX = a.x > b.x ? (a.x > c.x ? a.x : c.x) : (b.x > c.x ? b.x : c.x),
-        maxTY = a.y > b.y ? (a.y > c.y ? a.y : c.y) : (b.y > c.y ? b.y : c.y);
+    var minTX = ax < bx ? (ax < cx ? ax : cx) : (bx < cx ? bx : cx),
+        minTY = ay < by ? (ay < cy ? ay : cy) : (by < cy ? by : cy),
+        maxTX = ax > bx ? (ax > cx ? ax : cx) : (bx > cx ? bx : cx),
+        maxTY = ay > by ? (ay > cy ? ay : cy) : (by > cy ? by : cy);
 
     // z-order range for the current triangle bbox;
     var minZ = zOrder(minTX, minTY, minX, minY, invSize),
@@ -180,12 +194,12 @@ function isEarHashed(ear, minX, minY, invSize) {
     // look for points inside the triangle in both directions
     while (p && p.z >= minZ && n && n.z <= maxZ) {
         if (p !== ear.prev && p !== ear.next &&
-            pointInTriangle(a.x, a.y, b.x, b.y, c.x, c.y, p.x, p.y) &&
+            pointInTriangle(ax, ay, bx, by, cx, cy, p.x, p.y) &&
             area(p.prev, p, p.next) >= 0) return false;
         p = p.prevZ;
 
         if (n !== ear.prev && n !== ear.next &&
-            pointInTriangle(a.x, a.y, b.x, b.y, c.x, c.y, n.x, n.y) &&
+            pointInTriangle(ax, ay, bx, by, cx, cy, n.x, n.y) &&
             area(n.prev, n, n.next) >= 0) return false;
         n = n.nextZ;
     }
@@ -193,7 +207,7 @@ function isEarHashed(ear, minX, minY, invSize) {
     // look for remaining points in decreasing z-order
     while (p && p.z >= minZ) {
         if (p !== ear.prev && p !== ear.next &&
-            pointInTriangle(a.x, a.y, b.x, b.y, c.x, c.y, p.x, p.y) &&
+            pointInTriangle(ax, ay, bx, by, cx, cy, p.x, p.y) &&
             area(p.prev, p, p.next) >= 0) return false;
         p = p.prevZ;
     }
@@ -201,7 +215,7 @@ function isEarHashed(ear, minX, minY, invSize) {
     // look for remaining points in increasing z-order
     while (n && n.z <= maxZ) {
         if (n !== ear.prev && n !== ear.next &&
-            pointInTriangle(a.x, a.y, b.x, b.y, c.x, c.y, n.x, n.y) &&
+            pointInTriangle(ax, ay, bx, by, cx, cy, n.x, n.y) &&
             area(n.prev, n, n.next) >= 0) return false;
         n = n.nextZ;
     }
