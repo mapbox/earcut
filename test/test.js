2b60a06f7c893a735ee672476440ad26e460e234
Simon Reinhard <sreinhard@esri.com>
2021-07-07 13:53:29 +0200

    Simplify test case

--- test/test.js
+++ test/test.js
@@ -43,3 +43,8 @@ Object.keys(expected.triangles).forEach(function (id) {
         t.end();
     });
 });
+
+test('infinite-loop', function (t) {
+    earcut([1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 4, 1, 5, 1, 3, 2, 4, 2, 4, 1], [5], 2);
+    t.end();
+});
