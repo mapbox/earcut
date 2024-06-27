11175faceead0f7d518349e2ce14643c06974bb0
Vladimir Agafonkin <agafonkin@gmail.com>
2024-06-27 13:04:14 +0300

    use native tests & coverage instead of tape & c8

--- test/test.js
+++ test/test.js
@@ -1,30 +1,28 @@
 
-import test from 'tape';
+import test from 'node:test';
+import assert from 'node:assert/strict';
 import {earcut, flatten, deviation} from '../src/earcut.js';
 import fs from 'fs';
 
 const expected = JSON.parse(fs.readFileSync(new URL('expected.json', import.meta.url)));
 
-test('indices-2d', function (t) {
+test('indices-2d', () => {
     var indices = earcut([10, 0, 0, 50, 60, 60, 70, 10]);
-    t.same(indices, [1, 0, 3, 3, 2, 1]);
-    t.end();
+    assert.deepEqual(indices, [1, 0, 3, 3, 2, 1]);
 });
 
-test('indices-3d', function (t) {
+test('indices-3d', () => {
     var indices = earcut([10, 0, 0, 0, 50, 0, 60, 60, 0, 70, 10, 0], null, 3);
-    t.same(indices, [1, 0, 3, 3, 2, 1]);
-    t.end();
+    assert.deepEqual(indices, [1, 0, 3, 3, 2, 1]);
 });
 
-test('empty', function (t) {
-    t.same(earcut([]), []);
-    t.end();
+test('empty', () => {
+    assert.deepEqual(earcut([]), []);
 });
 
-Object.keys(expected.triangles).forEach(function (id) {
+for (const id of Object.keys(expected.triangles)) {
 
-    test(id, function (t) {
+    test(id, () => {
         var data = flatten(JSON.parse(fs.readFileSync(new URL(`fixtures/${id}.json`, import.meta.url)))),
             indices = earcut(data.vertices, data.holes, data.dimensions),
             err = deviation(data.vertices, data.holes, data.dimensions, indices),
@@ -32,18 +30,15 @@ Object.keys(expected.triangles).forEach(function (id) {
             expectedDeviation = expected.errors[id] || 0;
 
         var numTriangles = indices.length / 3;
-        t.ok(numTriangles === expectedTriangles, numTriangles + ' triangles when expected ' + expectedTriangles);
+        assert.ok(numTriangles === expectedTriangles, numTriangles + ' triangles when expected ' + expectedTriangles);
 
         if (expectedTriangles > 0) {
-            t.ok(err <= expectedDeviation,
-                'deviation ' + err + ' <= ' + expectedDeviation);
+            assert.ok(err <= expectedDeviation, 'deviation ' + err + ' <= ' + expectedDeviation);
         }
 
-        t.end();
     });
-});
+}
 
-test('infinite-loop', function (t) {
+test('infinite-loop', () => {
     earcut([1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 4, 1, 5, 1, 3, 2, 4, 2, 4, 1], [5], 2);
-    t.end();
 });
